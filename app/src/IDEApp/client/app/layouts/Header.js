f0b8e8799a4917c4bf68d0366b5dd4d519e18819
cdmbase <mvasireddy@cdmbase.com>
2016-08-12 06:56:55 -0400

    still have violation but removed duplicate react

--- app/src/IDEApp/client/app/layouts/Header.js
+++ app/src/IDEApp/client/app/layouts/Header.js
@@ -1,48 +1,34 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
-import { Row, Col, Nav, Navbar, NavbarBrand, NavItem} from 'react-bootstrap';
+import { Row, Col, Nav, Navbar, NavbarBrand, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import linksMessages from '../../../common/app/linksMessages';
 import { LinkContainer } from 'react-router-bootstrap';
 
 
-class Header extends Component {
+const Header = () => {
     //static propTypes = {
     //    viewer: PropTypes.object,
     //};
 
-    render() {
         //const { viewer } = this.props;
 
         return (
-            <Navbar>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a href="#">IDE</a>
-                    </Navbar.Brand>
-                </Navbar.Header>
-                <Nav pullRight>
-                    <LinkContainer to="/">
-                        <NavItem eventKey={1}>
-                            <FormattedMessage {...linksMessages.home} />
-                        </NavItem>
-                    </LinkContainer>
-                    <LinkContainer to="/pricing">
-                        <NavItem eventKey={2}>
-                            <FormattedMessage {...linksMessages.pricing} />
-                        </NavItem>
-                    </LinkContainer>
-                    <LinkContainer to="/signUp">
-                        <NavItem eventKey={3}>
-                            <FormattedMessage {...linksMessages.signIn} />
-                        </NavItem>
-                    </LinkContainer>
-                </Nav>
-            </Navbar>
-
+            <div id="nav_header">
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">React-Bootstrap</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">Link</NavItem>
+                        <NavItem eventKey={2} href="#">Link</NavItem>
+                    </Nav>
+                </Navbar>
+            </div>
         )
-    }
 }
 
 export default Header;
