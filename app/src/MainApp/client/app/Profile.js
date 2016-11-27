7c9ebd7bffb29a7c4056e22a1bc66598627fcfa8
veeramarni <mvasireddy@cdmbase.com>
2016-11-27 14:21:15 -0500

    added profile and accountsMeld

---
+++ app/src/MainApp/client/app/Profile.js
@@ -0,0 +1,19 @@
+import React, { PropTypes, Component } from 'react';
+import Tracker from 'tracker-component';
+import { Accounts, STATES } from 'meteor/std:accounts-bootstrap';
+import { browserHistory } from 'react-router';
+import { Meteor } from 'meteor/meteor';
+
+
+export default class Profile extends Tracker.Component {
+
+
+    render () {
+        return (
+            <div className="container">
+                <Accounts.ui.LoginForm  fromState={ STATES.PROFILE }/>
+            </div>
+        )
+    }
+
+};
